8d300ddb7ea7aafc5432469a7e9c6eeabe217ab1
IronJon <lang.jeffrey@gmail.com>
2020-11-22 15:58:48 -0700

    md file write working need to refine inquirer questions

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,47 +1,45 @@
 function generateMarkdown(data) {
-  return `# ${data.title}  
-  `;
+  return `# ${data.title}
+
+  ## Description 
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+${data.installation}
+
+## Usage 
+${data.usage}
+
+## License
+${data.license}
+//provide description for license.
+
+## Badges
+//get badges based on license type.
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+//prompt for github username, link to github account.
+${data.user}
+//add email address and instructions on how to reach me.
+${data.email}  
+`;
 }
+
 
 module.exports = generateMarkdown;
 
-// # Your Project Title
 
-// ## Description 
-
-
-// ## Table of Contents 
-
-// * [Installation](#installation)
-// * [Usage](#usage)
-// * [Credits](#credits)
-// * [License](#license)
-
-
-// ## Installation
-
-
-// ## Usage 
-
-
-
-// ## Credits
-
-
-// ## License
-
-// The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
-
-
-// ## Badges
-
-// ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-// ## Contributing
-
-
-// ## Tests
-
-// ## Questions
-// //prompt for github username, link to github account.
-// //add email address and instructions on how to reach me.
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const markdown = require('./generateMarkdown');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 const { async } = require('rxjs');
-const generateMarkdown = require('./generateMarkdown');
+const markdown = require('./generateMarkdown');
 
 // array of questions for user
 const questions = () => {
@@ -13,56 +12,47 @@ const questions = () => {
         name: 'title',
         message: 'Tell us the name of your project:',
     },
-    // {
-    //     type: 'input',
-    //     name: 'description',
-    //     message: 'Describe the what, why, and how of your project:',      
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'installation',
-    //     message: 'What are the steps to install your project?',      
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'usage',
-    //     message: 'How would someone use your project?',      
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'license',
-    //     message: 'What type of license would you like to have for this project?',      
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'license',
-    //     message: 'What type of license would you like to have for this project?',      
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'contribution',
-    //     message: 'How can people contribute to this project?',      
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'tests',
-    //     message: 'Please list any tests that you used for this project:',      
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'tests',
-    //     message: 'Please list any tests that you used for this project:',      
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'user',
-    //     message: 'What is your Github username?',      
-    // },
-    //     {
-    //     type: 'input',
-    //     name: 'email',
-    //     message: 'What is a good email to reach you at with questions on this project?',      
-    // },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the what, why, and how of your project:',      
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps to install your project?',      
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How would someone use your project?',      
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What type of license would you like to have for this project?',      
+    },
+ 
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can people contribute to this project?',      
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please list any tests that you used for this project:',      
+    },
+    {
+        type: 'input',
+        name: 'user',
+        message: 'What is your Github username?',      
+    },
+        {
+        type: 'input',
+        name: 'email',
+        message: 'What is a good email to reach you at with questions on this project?',      
+    },
 ]);
 };
 
@@ -78,9 +68,9 @@ const init = async () => {
     try {
     const answers = await questions();
 
-    const readme = generateMarkdown(answers);     
+    const readme = markdown(answers);     
 
-    await writeToFile('readme.md', readme);
+    await writeToFile(`${answers.title}.md`, readme);
     
     console.log('Check out your new .readme');
     } catch (err) {
